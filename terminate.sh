7cb1e9c70575bc9555a7bb996267fe784f7de99c
smit-sms <smit16@somaiya.edu>
2023-11-15 23:46:41 +0000

    feat: updated terminate script

--- terminate.sh
+++ terminate.sh
@@ -4,7 +4,7 @@
 MAIN_PY_PROCESS=$(ps aux | grep 'python3 ./main.py' | grep -v 'grep' | awk '{print $2}')
 
 # Find the process ID of 'python3 ./web.py'
-WEB_PY_PROCESS=$(ps aux | grep 'python3 ./web.py' | grep -v 'grep' | awk '{print $2}')
+# WEB_PY_PROCESS=$(ps aux | grep 'python3 ./web.py' | grep -v 'grep' | awk '{print $2}')
 
 # Initialize a counter for killed processes
 KILLED_COUNT=0
@@ -19,13 +19,13 @@ else
 fi
 
 # Kill the web.py process
-if [ ! -z "$WEB_PY_PROCESS" ]; then
-    echo "Killing process python3 ./web.py with PID $WEB_PY_PROCESS"
-    kill $WEB_PY_PROCESS
-    let KILLED_COUNT++
-else
-    echo "No process found for python3 ./web.py"
-fi
+# if [ ! -z "$WEB_PY_PROCESS" ]; then
+#     echo "Killing process python3 ./web.py with PID $WEB_PY_PROCESS"
+#     kill $WEB_PY_PROCESS
+#     let KILLED_COUNT++
+# else
+#     echo "No process found for python3 ./web.py"
+# fi
 
 # Check if both processes were killed
 if [ "$KILLED_COUNT" -eq 2 ]; then
